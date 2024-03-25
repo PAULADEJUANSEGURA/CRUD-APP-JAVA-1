d2ba3b9d27683dccb845c612f041f328ebdc6ce6
PAULADEJUANSEGURA <pauladejuansegura34@protonmail.com>
2024-03-25 12:21:20 +0100

    Nueva app para hacer un CRUD en JAVA

---
+++ src/main/java/com/teatro/app/TeatroAppApplication.java
@@ -0,0 +1,13 @@
+package com.teatro.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeatroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeatroAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teatro/app/TeatroAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teatro.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeatroAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
